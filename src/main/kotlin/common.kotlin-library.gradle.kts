f78e28d782fabdf8a84e1f95ebf93e5c7dce28f8
adriangacea <adrian.gacea@codecentric.de>
2020-12-17 17:26:46 +0100

    Initial commit

---
+++ src/main/kotlin/common.kotlin-library.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    `java-library`
+
+    id("common.kotlin-conventions")
+
+    id("com.github.ben-manes.versions")
+    id("com.github.johnrengelman.shadow")
+
+    id("cloud.maven-publishing")
+}
